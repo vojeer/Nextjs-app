7f9d1fdc3df09cd22797e482f9d2c648ac3f827f
wazir ansari <wazir.ansari@stylework.city>
2023-07-06 20:12:54 +0530

    feat:<intial commit todo list app>

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,8 @@
+import{AiOutlinePlus} from 'react-icons/ai'
+const AddTask = () => {
+    return <div>
+        <button className="btn btn-primary w-full">Add New Task 
+        <AiOutlinePlus className='ml-1' size={18}/></button>
+    </div>
+};
+export default AddTask;
